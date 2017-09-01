8aa5d9fea6794ad88f38388790725a4906e21c73
Rien Croonenborghs <rien@croonenborghs.net>
2017-09-01 14:44:12 +1200

    generate fix

--- app/img_flip.rb
+++ app/img_flip.rb
@@ -25,7 +25,7 @@ class ImgFlip
 
     response  = self.class.post!("/caption_image", params)
     if response.body["success"]
-      response.body["data"]["url"]
+      {image_url: response.body["data"]["url"]} 
     else
       response.body["error_message"]
     end
