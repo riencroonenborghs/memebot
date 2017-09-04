255981ee0f33515ff9da24dd5e4986360671b7e5
Rien Croonenborghs <rien@croonenborghs.net>
2017-09-04 17:49:10 +1200

    paginate list

--- app/slack/authorizer.rb
+++ app/slack/authorizer.rb
@@ -1,6 +1,6 @@
 module Slack
   class Authorizer
-    UNAUTHORIZED_MESSAGE = "Oops! Looks like the application is not authorized! Please review the token configuration."
+    UNAUTHORIZED_MESSAGE  = "Oops! Looks like the application is not authorized! Please review the token configuration."
     UNAUTHORIZED_RESPONSE = ["200", {"Content-Type" => "text"}, [UNAUTHORIZED_MESSAGE]]
 
     def initialize(app)
@@ -9,12 +9,17 @@ module Slack
     
     def call(env)
       req = Rack::Request.new(env)
-      if  req.path_info == "/list" || 
-          req.params["token"] == ENV["SLACK_TOKEN"]
+      if get_token(req) == ENV["SLACK_TOKEN"]
         @app.call(env)
       else
         UNAUTHORIZED_RESPONSE
       end
+    end
+
+  private
+
+    def get_token(req)
+      req.params["token"] || JSON.parse(req.params["payload"] || "{}")["token"]
     end
   end
 end
